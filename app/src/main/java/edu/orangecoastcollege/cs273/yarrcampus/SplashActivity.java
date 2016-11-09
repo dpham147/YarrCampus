ec3dc698f83dcc262a97d64507649faed0669c7e
dpham147 <dannypham92@hotmail.com>
2016-11-09 00:11:27 -0800

    Added MenuActivity and SplashActivity timer

--- app/src/main/java/edu/orangecoastcollege/cs273/yarrcampus/SplashActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/yarrcampus/SplashActivity.java
@@ -1,13 +1,34 @@
 package edu.orangecoastcollege.cs273.yarrcampus;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 5000;
+    private TextView splashTipTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        splashTipTextView = (TextView) findViewById(R.id.splashTipTextView);
+
+        TimerTask menuTask = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(SplashActivity.this, MenuActivity.class));
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(menuTask, SPLASH_DELAY);
     }
 }
