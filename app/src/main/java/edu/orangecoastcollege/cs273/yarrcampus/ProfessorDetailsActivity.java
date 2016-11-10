d9a37c4b0f17e697901ef77d3b7cd38dc42400c0
RomanB1224 <roman.jedi@yahoo.com>
2016-11-10 15:55:19 -0800

    added professor list and professor details xml and java class

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/yarrcampus/ProfessorDetailsActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.cs273.yarrcampus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfessorDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professor_details);
+    }
+}
