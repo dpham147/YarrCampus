23eb9b103dd943efddfc0ba730ec83171c944917
dpham147 <dannypham92@hotmail.com>
2016-11-10 16:13:51 -0800

    Connected menu intents

--- app/src/main/java/edu/orangecoastcollege/cs273/yarrcampus/MenuActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/yarrcampus/MenuActivity.java
@@ -1,5 +1,6 @@
 package edu.orangecoastcollege.cs273.yarrcampus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,17 +15,17 @@ public class MenuActivity extends AppCompatActivity {
 
     public void openBuildingSearchActivity(View view)
     {
-
+        startActivity(new Intent(this, BuidlingListActivity.class));
     }
 
     public void openProfessorSearchActivity(View view)
     {
-
+        startActivity(new Intent(this, ProfessorListActivity.class));
     }
 
     public void openUtilitySearchActivity(View view)
     {
-
+        startActivity(new Intent(this, UtilitySearchActivity.class));
     }
 }
 
---
+++ app/src/main/java/edu/orangecoastcollege/cs273/yarrcampus/UtilitySearchActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.cs273.yarrcampus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UtilitySearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utility_search);
+    }
+}
