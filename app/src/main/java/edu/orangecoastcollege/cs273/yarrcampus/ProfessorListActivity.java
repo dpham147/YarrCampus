4e45d3eae48d692aa3cbf2d3a6104f7d9792d58d
rbarron11 <roman.jedi@yahoo.com>
2016-11-15 16:52:12 -0800

    adding onTextChange to filter the list to display the professors who's name starts with the user inputs.

--- app/src/main/java/edu/orangecoastcollege/cs273/yarrcampus/ProfessorListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/yarrcampus/ProfessorListActivity.java
@@ -2,10 +2,15 @@ package edu.orangecoastcollege.cs273.yarrcampus;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
+
+
 
 public class ProfessorListActivity extends AppCompatActivity {
 
@@ -25,5 +30,30 @@ public class ProfessorListActivity extends AppCompatActivity {
 
         db = new DBHelper(this);
         professorsList = db.getAllProfessors();
+
+        private TextWatcher searchTextChangedListener = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            List<Professor> searchedProfessors = new ArrayList<Professor>();
+                for(Professor p : professorsList)
+                {
+                    if(p.getmName().startsWith(charSequence.toString()))
+                    {
+                        searchedProfessors.add(p);
+
+                    }
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        }
     }
 }
