cc38de762b01e27bf5618131f656938ae4835de9
dpham147 <dannypham92@hotmail.com>
2016-11-25 18:50:04 -0800

    Added QuizActivity, attempted GestureDetector options on MenuActivity

--- app/src/main/java/edu/orangecoastcollege/cs273/yarrcampus/MenuActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/yarrcampus/MenuActivity.java
@@ -3,15 +3,23 @@ package edu.orangecoastcollege.cs273.yarrcampus;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+
+    private int gameCounter = 0;
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         getSupportActionBar().hide();
+
+        gestureDetector = new GestureDetector(this, this);
+
     }
 
     public void openBuildingSearchActivity(View view)
@@ -27,6 +35,47 @@ public class MenuActivity extends AppCompatActivity {
     public void openUtilitySearchActivity(View view)
     {
         startActivity(new Intent(this, UtilitySearchActivity.class));
+    }
+
+    @Override
+    public  boolean dispatchTouchEvent(MotionEvent ev){
+        super.dispatchTouchEvent(ev);
+        return gestureDetector.onTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        gameCounter++;
+        if (gameCounter > 10)
+        {
+            startActivity(new Intent(this, QuizActivity.class));
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
     }
 }
 
