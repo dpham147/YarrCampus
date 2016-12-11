bcca10870734181318319f1afc694f3b7b5e7289
RomanB1224 <roman.jedi@yahoo.com>
2016-12-10 19:00:55 -0800

    added ProfessorMapActivity and layout

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/yarrcampus/ProfessorMapActivity.java
@@ -0,0 +1,120 @@
+package edu.orangecoastcollege.cs273.yarrcampus;
+
+import android.*;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ProfessorMapActivity extends AppCompatActivity
+        implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+
+    private static final int LOCATION_REQUEST_CODE = 100;
+
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private Location myLocation;
+    private LocationRequest mLocationRequest;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professor_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.officeMapFragment);
+        mapFragment.getMapAsync(this);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API).build();
+            mGoogleApiClient.connect();
+        }
+
+
+        // Define the interval for updates:
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10000)
+                .setFastestInterval(1000);
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+        }
+        myLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        handleNewLocation(myLocation);
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.e("Yarr Campus", "Suspended connection from Google Play Services.");
+    }
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e("Yarr Campus", "Failed connection from Google Play Services.");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+        }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+    }
+
+    public void handleNewLocation(Location location) {
+        Intent professorDetailsIntent = getIntent();
+        Professor selectedProfessor = professorDetailsIntent.getParcelableExtra("SelectedProfessor");
+
+        mMap.clear();
+        myLocation = location;
+        LatLng officeCoordinate = new LatLng(selectedProfessor.getmOfficeLat(), selectedProfessor.getmOfficeLong());
+        mMap.addMarker(new MarkerOptions()
+                .position(officeCoordinate)
+                .title("Current Location"));
+        //.icon(BitmapDescriptorFactory.fromResource(R.drawable.my_marker)));
+
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(officeCoordinate).zoom(14.0f).build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        mMap.moveCamera(cameraUpdate);
+
+        // Add my marker
+       LatLng coordinate = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(coordinate).title(selectedProfessor.getmName()));
+    }
+}
+
